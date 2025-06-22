9b8d7a17a87e6f0c07851772abea6f905d6b38d0
William Levesque <Willleves@icloud.com>
2025-06-22 17:37:11 -0400

    finished navbar adn header ish

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './Main/Main.jsx';
+import Game from './Game/Game.jsx';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="NavBarVRest">
-        <div className="Navbar">
-            <div className="Logo">
-                <img src="/logo.png" alt="Logo" />
-            </div>
-            <div className="NavLinks">
-                <a href="/">home</a>
-                <a href="/about">game</a>
-                <a href="/contact">contact</a>
-            </div>
-        <div className='Header'>
-          
-      </div>
+      <div className="AppLayout">
+        <nav className="Navbar">
+          <div className="Logo">
+            <div className="Ellipse"></div>
+          </div>
+          <div className="NavLinks">
+            <a href="/"><span className="NavLinkText">home</span></a>
+            <a href="/about"><span className="NavLinkText">play</span></a>
+          </div>
+        </nav>
+        <div className="MainContent">
+          <header className="Header">
+             <div className="HeaderBar top"></div>
+             <div className="HeaderBar middle"></div>
+             <div className="HeaderBar bottom"></div>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path='/Game' element={<Game />} />
+            </Routes>
+          </main>
         </div>
-
       </div>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
     </Router>
   );
 }
