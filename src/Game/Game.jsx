039977c5f713182fd8c8097774a3c24c69e2ff7c
William Levesque <Willleves@icloud.com>
2025-06-22 21:21:03 -0400

    created gamepage

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import './Game.css';
 const Game = () => {
     return (
-        <div></div>
+        <div className="GamePage">
+            <div className="GamePageBox">
+                <div className='CornerCircle1'>
+                <div className="CornerCircle1 top-left"></div>
+                <div className="CornerCircle1 top-right"></div>
+                <div className="CornerCircle1 bottom-left"></div>
+                <div className="CornerCircle1 bottom-right"></div>
+                </div>
+                <h1 className="TheGame"> The Game </h1>
+                <button className="StartButton">Start</button>
+                <button className="SettingsButton">settings</button>
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
 
     );
     
